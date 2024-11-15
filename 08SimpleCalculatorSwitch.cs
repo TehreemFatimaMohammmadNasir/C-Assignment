5afd60c32566e235ca3d77dae4b92b5a203f5b48
tehreem fatima <163853253+TehreemFatimaMohammmadNasir@users.noreply.github.com>
2024-11-15 22:01:39 +0500

    Update 08SimpleCalculatorSwitch.cs

--- 08SimpleCalculatorSwitch.cs
+++ 08SimpleCalculatorSwitch.cs
@@ -12,15 +12,15 @@ class Calculator
     static void Main()
     {
         
-        Console.Write("Enter the first number: ");
+        Console.WriteLine("Enter the first number: ");
         double num1 = Convert.ToDouble(Console.ReadLine());
 
        
-        Console.Write("Enter the second number: ");
+        Console.WriteLine("Enter the second number: ");
         double num2 = Convert.ToDouble(Console.ReadLine());
 
       
-        Console.Write("Enter the operation (+, -, *, /)");
+        Console.WriteLine("Enter the operation (+, -, *, /)");
         char operation = Convert.ToChar(Console.ReadLine());
 
         
