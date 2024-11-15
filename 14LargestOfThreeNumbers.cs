0baefa1fa85b31a460f7869313bc3ffb23331ad9
tehreem fatima <163853253+TehreemFatimaMohammmadNasir@users.noreply.github.com>
2024-11-15 22:05:41 +0500

    Update 14LargestOfThreeNumbers.cs

--- 14LargestOfThreeNumbers.cs
+++ 14LargestOfThreeNumbers.cs
@@ -12,13 +12,13 @@ class Program
     static void Main()
     {
       
-        Console.Write("Enter the first number: ");
+        Console.WriteLine("Enter the first number: ");
         int num1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter the second number: ");
+        Console.WriteLine("Enter the second number: ");
         int num2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter the third number: ");
+        Console.WriteLine("Enter the third number: ");
         int num3 = Convert.ToInt32(Console.ReadLine());
 
         
