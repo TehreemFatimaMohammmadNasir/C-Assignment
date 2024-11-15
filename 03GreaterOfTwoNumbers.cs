bc5330554c15c2bf7123a4127c82af082441057e
tehreem fatima <163853253+TehreemFatimaMohammmadNasir@users.noreply.github.com>
2024-11-15 21:59:33 +0500

    Update 03GreaterOfTwoNumbers.cs

--- 03GreaterOfTwoNumbers.cs
+++ 03GreaterOfTwoNumbers.cs
@@ -11,10 +11,10 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter the first number: ");
+        Console.WriteLine("Enter the first number: ");
         int num1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter the second number: ");
+        Console.WriteLine("Enter the second number: ");
         int num2 = Convert.ToInt32(Console.ReadLine());
 
           if (num1 > num2)
